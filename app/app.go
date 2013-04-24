975e9b7456935e84b50d4f7b85f6275e78de8e23
Michael Neale <michael.neale@gmail.com>
2013-04-24 20:51:18 +1000

    fixed to have index.html

--- app/app.go
+++ app/app.go
@@ -10,6 +10,7 @@ import (
        "net/http"
        "regexp"
        "os"
+       "fmt"
 )
 
 type Page struct {
@@ -39,6 +40,8 @@ func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
              }
              renderTemplate(w, "view", p)
 }
+
+
 
 func editHandler(w http.ResponseWriter, r *http.Request, title string) {
      p, err := loadPage(title)
@@ -83,9 +86,51 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
                                                 }
 }
 
+
+
+
 func main() {
+     http.HandleFunc("/", index)
      http.HandleFunc("/view/", makeHandler(viewHandler))
      http.HandleFunc("/edit/", makeHandler(editHandler))
      http.HandleFunc("/save/", makeHandler(saveHandler))
      http.ListenAndServe(":" + os.Getenv("PORT"), nil)
+}
+
+
+func index(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, 
+        `<html><h1><code>Welcome to Go Lang on <a href='http://developer.CloudBees.com'>CloudBees</a></code></h1>
+        <code>Made with a <a href='https://github.com/CloudBees-community/golang-clickstart'>ClickStart</a></code><p>
+        <p>
+            <code>Create a new <a href="/view/test">wiki page</a><code>
+        </p>
+
+        <!-- this is used for first time display of clickstart - a template-->
+    <div id="clickstart_content" style="display:none">
+    <p>
+      Congratulations, your <a href="#CS_docUrl"><span>#CS_name</span></a> application is now running.<br />
+      To modify it, <a href="https://grandcentral.cloudbees.com/user/ssh_keys">
+      upload your public key (for git) here</a> if you haven't already.
+      <br>Then clone your project:
+    </p>
+    <div class="CB_codeSample">
+      git clone #CS_source #CS_appName<br/>
+          cd #CS_appName<br/>
+          ---- do your magic edits ----<br/>
+          git commit -m "This is now even better"<br/>
+          git push origin master
+    </div>
+    <p>That is it ! This will trigger your build/deploy pipline and publish your change</p>
+    <p>We have set up all the moving parts for you - the management urls can be found on the following urls:</p>
+    <ul>
+      <li><strong>App console:</strong> <a href="#CS_appManageUrl">#CS_appManageUrl</a></li>
+      <li><strong>Jenkins Build System:</strong> <a href="#CS_jenkinsUrl">#CS_jenkinsUrl</a></li>
+      <li><strong>Source repositories:</strong> <a href="#CS_forgeUrl">#CS_forgeUrl</a></li>
+    </ul>
+  </div>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.0/jquery.min.js"></script>
+  <script type="text/javascript" src="https://s3.amazonaws.com/cloudbees-downloads/clickstart/clickstart_intro.js"></script>
+  <!-- end clickstart intro section -->     
+    </html>`)
 }
